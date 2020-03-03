b79a0aa36272e8e41d26fd9ab1a2013b136ecfd6
ariful7885 <arifulislam2376@gmail.com>
2020-03-03 22:59:58 +0600

    complete the practise of ES6

---
+++ class.js
@@ -0,0 +1,16 @@
+class Student {
+constructor(sID,sName){
+    this.id = sID;
+    this.name = sName;
+    this.school = "gopaldi girls high school";
+}
+}
+
+const student1 = new Student(12,"karim");
+const student2 = new Student(15,"mahe");
+const student3 = new Student(150,"maruf");
+console.log(student3);
+console.log(student1,student2);
+console.log(student1.name,student2.id);
+
+
